fbc8761dbe8cd4c6f817b938c64fcaa88b5812cd
Batyr-R <batiaka39@gmail.com>
2025-08-04 23:55:12 +0500

    add token counts and a fix

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 print(response.text)
-print(responce.usage_metadata)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
