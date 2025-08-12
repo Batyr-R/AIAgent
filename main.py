9de71e1eb577bdaac6790f44df52a10422f7adf9
Batyr-R <batiaka39@gmail.com>
2025-08-12 16:16:08 +0500

    add system prompt and function declaration

--- main.py
+++ main.py
@@ -18,10 +18,43 @@ if len(sys.argv) < 2:
     print("Error: prompt not provided")
     sys.exit(1)
 
+schema_get_files_info = genai.types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=genai.types.Schema(
+        type=genai.types.Type.OBJECT,
+        properties={
+            "directory": genai.types.Schema(
+                type=genai.types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = genai.types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 prompt = arguments.prompt
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=prompt
+    model='gemini-2.0-flash-001',
+    contents=prompt,
+    config=genai.types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    ),
 )
 
 if arguments.verbose:
@@ -29,4 +62,8 @@ if arguments.verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
+for p in response.candidates[0].content.parts:
+    if p.function_call != None:
+            print(f"Calling function: {p.function_call.name}({p.function_call.args})")
+    else:
+        print(p.text)
